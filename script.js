3d743ce1490daa09b149cf3c05b5c2b6a310d5e4
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-21 15:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-//your code here
 // Implementing myCall
 Function.prototype.myCall = function(thisContext, ...args) {
-  
+  if (typeof this !== "function") {
+    throw new TypeError("myCall must be called on a function");
+  }
+
+  const key = Symbol();            
+  thisContext[key] = this;         
+
+  const result = thisContext[key](...args); 
+
+  delete thisContext[key];         
+  return result;
+};
+
+// Implementing myApply
+Function.prototype.myApply = function(thisContext, args) {
+  if (typeof this !== "function") {
+    throw new TypeError("myApply must be called on a function");
+  }
+
+  const key = Symbol();
+  thisContext[key] = this;
+
+  let result;
+  if (Array.isArray(args)) {
+    result = thisContext[key](...args);
+  } else {
+    result = thisContext[key](); 
+  }
+
+  delete thisContext[key];
+  return result;
+};
+
+// Implementing myBind
+Function.prototype.myBind = function(thisContext, ...args) {
+  if (typeof this !== "function") {
+    throw new TypeError("myBind must be called on a function");
+  }
+
+  const originalFn = this;
+
+  return function(...laterArgs) {
+    const key = Symbol();
+    thisContext[key] = originalFn;
+
+    const result = thisContext[key](...args, ...laterArgs);
+
+    delete thisContext[key];
+    return result;
   };
-  
-  // Implementing myApply
-  Function.prototype.myApply = function(thisContext, args) {
-    
-  
-  };
-  
-  // Implementing myBind
-  Function.prototype.myBind = function(thisContext, ...args) {
-    
-  };
-  
-  module.exports = Function.prototype;
+};
+
+module.exports = Function.prototype;
